b16ac1ae68d89a646c4c3697c56dcb931977353f
Henry Shin <bg22915@bristol.ac.uk>
2024-11-01 11:26:31 +0000

    test again

--- gol-skeleton-master-distributed/broker/broker.go
+++ gol-skeleton-master-distributed/broker/broker.go
@@ -244,9 +244,9 @@ func (b *Broker) StopProcessing(req *stubs.StopRequest, res *stubs.StopResponse)
 
 func main() {
 	workerAddrs := []string{
-		"3.81.85.114:8031",
-		"54.87.162.225:8032",
-		"34.207.197.90:8033",
+		"3.80.176.216:8031",
+		"3.81.127.44:8032",
+		"54.211.79.254:8033",
 		// Add more worker addresses as needed
 	}
 
