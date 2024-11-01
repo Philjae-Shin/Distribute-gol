76221b3516e6b8bb44a055cdc2d435ae4e4d9418
Henry Shin <bg22915@bristol.ac.uk>
2024-11-01 01:12:56 +0000

    Update benchmark_test.go

--- gol-skeleton-master-distributed/benchmark_test.go
+++ gol-skeleton-master-distributed/benchmark_test.go
@@ -17,11 +17,19 @@ func BenchmarkGol(b *testing.B) {
 
 	for threads := 1; threads <= 16; threads++ {
 		b.Run(fmt.Sprintf("%d_workers", threads), func(b *testing.B) {
+			//traceParams := gol.Params{
+			//	Turns:       100,
+			//	Threads:     threads,
+			//	ImageWidth:  512,
+			//	ImageHeight: 512,
+			//}
+
+			//Benchmark for 5120x5120.pgm
 			traceParams := gol.Params{
 				Turns:       100,
 				Threads:     threads,
-				ImageWidth:  512,
-				ImageHeight: 512,
+				ImageWidth:  5120,
+				ImageHeight: 5120,
 			}
 			events := make(chan gol.Event)
 			for i := 0; i < b.N; i++ {
